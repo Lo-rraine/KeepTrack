e0f85dae6c6cf52178a8ea62acf34a98aa3a2e29
Lorrainem <moyolorraine98@gmail.com>
2022-10-25 18:58:45 +0200

    Scaffolded Workouts and Personal trainers

---
+++ KeepTrack/Pages/AthleteWorkouts/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KeepTrack.Data;
+using KeepTrack.Models;
+
+namespace KeepTrack.Pages.AthleteWorkouts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly KeepTrack.Data.ApplicationDbContext _context;
+
+        public CreateModel(KeepTrack.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["AthleteId"] = new SelectList(_context.Athletes, "ID", "ID");
+        ViewData["WorkoutId"] = new SelectList(_context.Workouts, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public AthleteWorkout AthleteWorkout { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.AthleteWorkouts.Add(AthleteWorkout);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ KeepTrack/Pages/Workouts/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using KeepTrack.Data;
+using KeepTrack.Models;
+
+namespace KeepTrack.Pages.Workouts
+{
+    public class CreateModel : PageModel
+    {
+        private readonly KeepTrack.Data.ApplicationDbContext _context;
+
+        public CreateModel(KeepTrack.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["PersonalTrainerId"] = new SelectList(_context.PersonalTrainers, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public Workout Workout { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Workouts.Add(Workout);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
